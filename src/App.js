6771621e8fa4c55f0fb6da77766743d93dd9df50
Diksi Erlangga <diksi131@gmail.com>
2019-07-20 23:02:30 +0800

    Render pakai es6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>Ricardo</div>
-        )
-    }
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <div>Ricardo</div>
+//         )
+//     }
+// }
+
+const App = ()=>{
+    return(
+        <div>OK</div>
+    )
 }
 
 export default App
