de54b12cd965364a384e275d8784bd737dadf097
Diksi Erlangga <diksi131@gmail.com>
2019-07-20 22:58:40 +0800

    Render pakai component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
-imp
+import React from 'react'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>Ricardo</div>
+        )
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from 'react'
 import ReactDom from 'react-dom'
+import App from './App'
 
-ReactDom.render(<div>Hello World</div>, 
+ReactDom.render(<App/>, 
         document.getElementById('root')
     )
